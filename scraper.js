584b3399a644467e685551be3319da2162b359ff
wojtek2kdev <wojtek2kdev@gmail.com>
2019-04-16 21:02:05 +0200

    Update scraper   - Translate word types into english (default it's polish)   - Create translations extractor

--- scraper.js
+++ scraper.js
@@ -2,7 +2,20 @@ import $ from 'cheerio';
 import { get } from 'axios';
 import { flow } from 'lodash/fp';
 
-const fetchTranslation = async ({from, to, args}) => {
+const translationTypes = {
+	"męski": "noun",
+	"żeński": "noun",
+	"nijaki": "noun",
+	"czasownik": "verb",
+	"przymiotnik": "adjective",
+	"przyimek": "preposition",
+	"zaimek": "pronoun",
+	"przysłówek": "adverb",
+	"wykrzyknik": "vocative",
+	"spójnik": "conjunction",
+};
+
+const fetchTranslation = async ({from, to, word}) => {
 	const baseURL = 'https://pl.bab.la/slownik';
 
 	const {data} = await get(`https://pl.bab.la/slownik/${from}-${to}/${word}`, {
@@ -15,7 +28,23 @@ const fetchTranslation = async ({from, to, args}) => {
 }; 
 
 const extractTranslations = (document) => {
-	const result = {};
+	const result = {
+		translations: {},
+	};
+
+	const translations = $('div[id^=translation]', document);
+
+	if(translations.length){
+		translations.each((index, element) => {
+			const type = $('div>.result-block-header>h3>.suffix' ,element).text().replace(/{|}/g, '');
+			const words = [];
+			$('.sense-group>div>.dict-translation>.dict-result>a>strong', element).each((i, word) => {
+				word = $(word).text();
+				words.push(word);
+			}); 
+			result.translations[translationTypes[type]] = words; 
+		});	
+	}
 
 	return {
 		document,
@@ -36,7 +65,17 @@ const extractSynonyms = ({document, result}) => {
 
 const scraping = async (args) => {
 	const document = await fetchTranslation(args);
-
 	
+	const { result } = extractTranslations(document);
 
+	console.log(result);
 };
+
+(async() => {
+	await scraping({
+		from: 'polski',
+		to: 'angielski',
+		word: 'jak',
+	});
+})();
+
